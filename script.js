d019011869c6732d014c15164dcdbe67e0a6132a
SYSHIL <ilhan.syed@gmail.com>
2022-03-17 11:31:07 +0530

    ADD UI

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //function randomly returns rock paper or scissors
-playround = (computerChoice,userChoice) => {
+function computerPlay(){
+    return (Math.floor(Math.random()*3)+1);
+}
+function gameTime(){
     let computerVictory = "Computer wins !";
     let userVictory = "User wins !";
     let tie = "It's a tie !";
     let invalidChoice = "Invalid choice !";
+
     if(userChoice > 3 || userChoice < 1){
         return invalidChoice;
     }
@@ -39,18 +43,25 @@ playround = (computerChoice,userChoice) => {
         }
     }
 }
-function computerPlay(){
-    return (Math.floor(Math.random()*3)+1);
+
+function playRound(){
+    userChoice = this.dataset.key;
+    computerChoice = computerPlay();
+    alert(gameTime(userChoice,computerChoice));
 }
-function play(noOfRounds){
+/*function play(noOfRounds){
     for(let i=0;i<noOfRounds;i++){
     let userChoice = prompt('Enter 1 for stone, 2 for paper, 3 for scissors');
     let computerChoice = computerPlay();
     alert(playround(computerChoice,userChoice));
     }
 }
-alert("Here's a cool game of stone paper scissors !");
-let noOfRounds = prompt("How many rounds would you like to play?");
 play(noOfRounds);
 alert("Thank you for playing !");
+*/
 
+const options = document.querySelectorAll(".UI button");
+options.forEach(option => {
+    option.addEventListener("click",playRound);
+
+});
