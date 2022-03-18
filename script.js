b0bf8983d1de3f9fa1ea2d4ae7446f339b833da8
SYSHIL <ilhan.syed@gmail.com>
2022-03-18 17:37:23 +0530

    UPDATE Score tab

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ function computerPlay(){
 function gameTime(userChoice){
     let computerChoice = computerPlay();
     //console.log(computerChoice,userChoice);
-    let computerVictory = "Computer wins !";
-    let userVictory = "User wins !";
-    let tie = "It's a tie !";
-    let invalidChoice = "Invalid choice !";
+    let computerVictory = "Computer";
+    let userVictory = "User";
+    let tie = "Nobody";
 
     if(computerChoice==1){//computer chooses rock
         if(userChoice==2) // user chooses paper
@@ -42,14 +41,47 @@ function gameTime(userChoice){
         }
     }
 }
-let gameResult;
+let noOfRounds = 0;
+let userWins = 0;
+let computerWins= 0;
+function updateScores(winner){
+    if(winner == "User")
+        userWins = userWins+1;
+    else if(winner == "Computer")
+        computerWins = computerWins+1;
+    const scores = document.querySelectorAll(".score>*")
+    scores[1].innerText = "User : " + userWins;
+    scores[2].innerText = "Computer : " + computerWins;
+}
+function displayWinner(){
+    const winner = document.querySelectorAll(".score>*");
+    if(userWins>computerWins) {
+        winner[3].innerText = "Winner : User";
+    }
+    else if(userWins<computerWins){
+        winner[3].innerText = "Winner : Computer";
+
+    }
+    else{
+        winner[3].innerText = "Winner : Nobody !"
+    }
+}
 function playRound(){
+    
     let userChoice = this.dataset.key;
     gameResult = gameTime(userChoice);
-    const body = document.querySelector('body');
-    const result = document.createElement("p");
-    result.innerText = gameResult;
-    body.appendChild(result);
+    const side = document.querySelector('.side');
+    const result = document.createElement("span");
+    result.innerText = gameResult + " won!";
+    side.appendChild(result);
+    updateScores(gameResult);
+    noOfRounds++;    
+    if(noOfRounds==5){
+        displayWinner();
+        options.forEach(option => {
+            option.removeEventListener("click",playRound);
+        });
+    }
 }
 
 const options = document.querySelectorAll(".UI button");
